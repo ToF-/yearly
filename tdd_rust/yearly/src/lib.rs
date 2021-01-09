03894913b4cd37ed25dd643cbf5626d0439132d2
Christophe Thibaut <cthibaut@octo.com>
2021-01-09 10:49:40 +0100

    date within a period

--- tdd_rust/yearly/src/lib.rs
+++ tdd_rust/yearly/src/lib.rs
@@ -5,6 +5,11 @@ mod date {
     pub fn a_day_earlier(date: Date<Utc>) -> Option<Date<Utc>> {
         date.checked_sub_signed(Duration::days(1))
     }
+
+    pub fn within(period: (Date<Utc>, Date<Utc>), date: Date<Utc>) -> bool {
+        let (start, end) = period;
+        date >= start && date <= end 
+    }
 }
 
 #[cfg(test)]
@@ -21,5 +26,15 @@ mod tests {
     #[test]
     fn a_day_earlier_should_be_equal_to_a_date_minus_1_day() {
         assert_eq!(a_day_earlier(Utc.ymd(2020,02,29)), Some(Utc.ymd(2020,02,28)));
+    }
+
+    #[test]
+    fn a_date_can_be_within_a_period() {
+        let period = (Utc.ymd(2020, 01, 01), Utc.ymd(2020,12,31));
+        assert_eq!(within(period,Utc.ymd(2020,03,31)),true);
+        assert_eq!(within(period,Utc.ymd(2019,03,31)),false);
+        assert_eq!(within(period,Utc.ymd(2021,03,31)),false);
+        assert_eq!(within(period,Utc.ymd(2020,01,01)),true);
+        assert_eq!(within(period,Utc.ymd(2020,12,31)),true);
     }
 }
